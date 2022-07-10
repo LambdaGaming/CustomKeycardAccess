b66da4c0c44d65855daa28c5bdaab31482f9ad47
LambdaGaming <starwars18m@gmail.com>
2022-07-10 17:32:42 -0400

    Assign version number the proper way

--- Plugin.cs
+++ Plugin.cs
@@ -1,11 +1,14 @@
 ﻿using Exiled.API.Features;
+using System;
 using events = Exiled.Events.Handlers;
 
 namespace CustomKeycardAccess
 {
-    public class Plugin : Plugin<Config>
-    {
-        private EventHandlers EventHandlers;
+	public class Plugin : Plugin<Config>
+	{
+		private EventHandlers EventHandlers;
+		public override Version Version { get; } = new Version( 1, 1, 0 );
+		public override Version RequiredExiledVersion { get; } = new Version( 5, 0, 0 );
 
 		public override void OnEnabled()
 		{
